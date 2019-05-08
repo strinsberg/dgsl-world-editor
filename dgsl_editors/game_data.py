34e2ffdc642ac289929c4f6fd97c53396ebb0297
strinsberg <deutekom@uleth.ca>
2019-05-08 02:32:04 +0000

    Added make functions for events and conditions

--- dgsl_editors/game_data.py
+++ dgsl_editors/game_data.py
@@ -1,20 +1,19 @@
 import uuid
 
+# Entity #######################################################
 
 def make_entity(kind="entity"):
     return {
         "id": uuid.uuid4(),
-        "type": "entity",
+        "type": kind,
         "name": "",
         "description": "",
         "events": [],
         "items": [],
     }
 
-
 def is_container(entity):
     return entity.kind in ["container", "room", "npc", "player"]
-
 
 def entity_string(entity):
     string = []
@@ -22,3 +21,92 @@ def entity_string(entity):
         if k not in ["items", "events"]:
             string.append(k + "=" + v + ";;")
     return str.join("\n", string)
+
+
+# Event ########################################################
+
+def make_event(kind, verb=None):
+    event = {
+        "id": uuid.uuid4(),
+        "type": kind,
+        "name": "",
+        "subjects": [],
+        "once": False,
+    }
+    if verb:
+        event["verb"] = verb
+    return event
+
+def make_inform(kind="inform", verb=None):
+    event = make_event(kind, verb)
+    event["message"] = ""
+    return event
+
+def make_kill(verb=None):
+    event = make_inform("kill", verb)
+    event["ending"] = False
+    return event
+
+def make_transfer(verb=None):
+    event = make_event("transfer", verb)
+    event["other"] = None
+    event["toTarget"] = False
+    event["item"] = None
+    return event
+
+def make_toggle(verb=None):
+    event = make_event("toggle", verb)
+    event["target"] = None
+    return event
+
+def make_move_player(verb=None):
+    event = make_event("move", verb)
+    event["destination"] = None
+    return event
+
+
+# Group event ##################################################
+
+def make_group(kind="group", verb=None):
+    event = make_event(kind, verb)
+    event["events"] = [],
+    event["repeats"] = False,
+    return event
+
+def make_interaction(verb=None):
+    event = make_event("interaction", verb)
+    event["options"] = {}
+    event["breakout"] = False
+    return event
+
+def make_cond_event(verb=None):
+    event = make_event("conditional", verb)
+    event["condition"] = None
+    event["success"] = None
+    event["failure"] = None
+    return event
+
+
+# Condition ####################################################
+
+def make_condition(subtype):
+    return {
+        "subtype": subtype,
+    }
+
+def make_has_item():
+    cond = make_condition("hasItem")
+    cond["itemId"] = ""
+    cond["other"] = None
+    return cond
+    
+def make_protected():
+    cond = make_condition("protected")
+    cond["atmosphere"] = ""
+    return cond
+
+def make_question():
+    cond = make_condition("question")
+    cond["question"] = ""
+    cond["answer"] = ""
+    return cond
