f51071e1165285209f566222e05dc36e631701cb
strinsberg <deutekom@uleth.ca>
2019-05-12 04:04:20 +0000

    Added bones for InfoEditor and factory for same. untested

---
+++ src/InfoEditor.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from SimpleDialog import SimpleDialog
+
+
+class InfoEditor(SimpleDialog):
+    def __init__(self, parent, self.obj):
+        SimpleDialog.__init__(self, parent)
+    
+    def makeWidgets(self, body):
+        pass
+    
+    def addEntry(self, label, kind):
+        pass
+    
+    def addCheck(self, label, state):
+        pass
+    
+    def addOption(self, label, options, first):
+        pass
+    
+    def apply():
+        pass
